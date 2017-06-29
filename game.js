04a8802f74c05f31818cd8cfa2bfab8bf04d915f
José Henrique Soares Silva <henrique.developer.net@gmail.com>
2017-06-29 01:56:04 -0300

    Versão com as imagens alterada para svg e colisão com as moedas

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 $(function () {
 
-    document.getElementById("musica").volume=0.1;
+    document.getElementById("musica").volume = 0.0;
+    document.getElementById("somMoeda").volume = 0.0;
     var grupoMoedas = {
-        timerUltimoGrupo:0
+        timerUltimoGrupo: 0
     }
-    
+
     var KEY = {
         W: 87,
         S: 83,
@@ -17,6 +18,10 @@ $(function () {
     player.pressedKeys = [];
     player.timer = setInterval(gameLoop, 30);
     player.ultimoTiro = 0;
+    player.ultimoTimerColisao = 0;
+    player.vidas = 3;
+    player.qtdMoedas=0;
+    player.qtdMoedasVida = 0;
 
     $(document).keydown(function (e) {
         player.pressedKeys[e.which] = true;
@@ -47,14 +52,12 @@ $(function () {
         }
     }
 
-    function moveGrupoMoedas()
-    {
-         if (Math.floor(Date.now()) > grupoMoedas.timerUltimoGrupo + 2000 || grupoMoedas.timerUltimoGrupo==0)
-         {
-             var objMoeda = new moeda();
-             grupoMoedas.timerUltimoGrupo = Math.floor(Date.now());
-            
-         }
+    function moveGrupoMoedas() {
+        if (Math.floor(Date.now()) > grupoMoedas.timerUltimoGrupo + 5000 || grupoMoedas.timerUltimoGrupo == 0) {
+            var objMoeda = new moeda();
+            grupoMoedas.timerUltimoGrupo = Math.floor(Date.now());
+
+        }
     }
 
     function playerAtirar() {
@@ -62,23 +65,76 @@ $(function () {
             || player.ultimoTiro == 0) {
             player.ultimoTiro = Math.floor(Date.now());
             $("#player1")
-                .removeClass("playerVoando")
-                .addClass("playerAtirando")
-                ;
+                .removeClass("voando")
+                .addClass("atirando");
             var objTiro = new tiro();
             setTimeout(playerVoar, 400);
         }
     }
     function playerVoar() {
         $("#player1")
-            .removeClass("playerAtirando")
-            .addClass("playerVoando")
+            .removeClass("atirando")
+            .addClass("voando")
             ;
 
     }
+
+    function getPosicao(elemento) {
+        var elemento = $(elemento);
+        var pos = elemento.position();
+        var width = (elemento.attr("class") == "player voando") ? elemento.width() - 25 : elemento.width();
+        var height = elemento.height();
+        return [
+            [pos.left, pos.left + width],
+            [pos.top, pos.top + height]
+        ];
+    }
+
+    function compararPosicao(p1, p2) {
+        var x1 = p1[0] < p2[0] ? p1 : p2;
+        var x2 = p1[0] < p2[0] ? p2 : p1;
+        return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
+    }
+
     function gameLoop() {
         movePlayer();
         moveGrupoMoedas();
+        checarColisoesMoedas();
     }
 
+    function checarColisoesMoedas() {
+        if (Math.floor(Date.now()) > player.ultimoTimerColisao + 100 || player.ultimoTimerColisaoo == 0) {
+
+            player.ultimoTimerColisao = Math.floor(Date.now());
+
+            $(".moeda").each(function (index, elem) {
+                if (checarColisao($(".player"), elem)) {
+                    document.getElementById("somMoeda").play();
+                    $(elem).remove();
+                    player.qtdMoedas++;
+                    player.qtdMoedasVida++;
+                    if(player.qtdMoedasVida>=10)
+                    {
+                        player.vidas++;
+                        player.qtdMoedasVida = 0;
+                    }
+                    $(".qtdMoedas").html("moedas " + player.qtdMoedas + "x");
+                    $(".vidas").html("vidas " + player.vidas + "x");
+                }
+
+            });
+        }
+
+    }
+
+
+    function checarColisao(elemento1, elemento2) {
+        var elemento1 = elemento1;
+        var pos = getPosicao(elemento1);
+
+        var pos2 = getPosicao(elemento2);
+        var horizontalMatch = compararPosicao(pos[0], pos2[0]);
+        var verticalMatch = compararPosicao(pos[1], pos2[1]);
+        return horizontalMatch && verticalMatch;
+    }
 });
--- moeda.js
+++ moeda.js
@@ -2,12 +2,13 @@ function moeda() {
     var velociadade = 6000;
     var quantidade = Math.floor((Math.random() * 10));
     var eixoY = Math.floor((Math.random() * 500));
-    var elementoGrupoMoedas = $("<div>", { class: "grupoMoedas", style: "top:" + eixoY + ";" });
-    for (var i = 0; i <= quantidade; i++) {
-        elementoGrupoMoedas.append($("<div>", { class: "moeda" }));
+    var elementoGrupoMoedas = [];
+    for (var i = 1; i <= quantidade; i++) {
+        elementoGrupoMoedas[i] = $("<div>", { class: "moeda", style: "left:" + (1300 + (39 * i)) + "; top:" + eixoY + ";" });
+        $(".cena").append(elementoGrupoMoedas[i]);
+        $(elementoGrupoMoedas[i]).animate({ "left": "-=2000px" }, velociadade, "linear", function () {
+            this.remove();
+        })
     }
-    $(".cena").append(elementoGrupoMoedas);
-    elementoGrupoMoedas.animate({ "left": "-=2000px" }, velociadade, "linear", function () {
-        elementoGrupoMoedas.remove();
-    });
+
 }
